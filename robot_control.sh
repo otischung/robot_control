7e2c07aa3e7ee8a8b6137dffb05d55985e08e434
otischung <my072814638@gmail.com>
2024-04-25 10:03:43 +0800

    [FEAT] Add ttyUSB 2-4

--- robot_control.sh
+++ robot_control.sh
@@ -10,5 +10,17 @@ if [ -e /dev/ttyUSB1 ]; then
     device_options+=" --device=/dev/ttyUSB1"
 fi
 
+if [ -e /dev/ttyUSB2 ]; then
+    device_options+=" --device=/dev/ttyUSB2"
+fi
+
+if [ -e /dev/ttyUSB3 ]; then
+    device_options+=" --device=/dev/ttyUSB3"
+fi
+
+if [ -e /dev/ttyUSB4 ]; then
+    device_options+=" --device=/dev/ttyUSB4"
+fi
+
 # 建構完整指令
 docker run -it --rm -v "$(pwd)/src:/workspaces/src" --network host $device_options --env-file ./.env --hostname pros-ai-image ghcr.io/otischung/pros_ai_image:latest /bin/bash
